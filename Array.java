9d5b239afaf759ac3aae1a24a10db244917f9585
jinka-sagar <sagarjinka12@gmail.com>
2025-04-08 18:31:41 +0530

    mysec commit

--- Array.java
+++ Array.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
-
-public class Array {
+public class Array{
     public static void main(String[] args) {
-       int[] arr=new int[5];
-        arr[0]=1;
-        arr[1]=2;
-        arr[2]=3;
-        arr[3]=4;
-        arr[4]=5;
-        System.out.println(arr);
+        int[] arr=new int[5];
+        Scanner in = new Scanner(System.in);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
